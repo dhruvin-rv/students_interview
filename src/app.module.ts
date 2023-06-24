16b05704edf36a1948ab77acdc6729d96116ebca
Dhruvin vaghasiya <dhruvinvaghasiya11@gmail.com>
2023-06-24 12:43:47 +0530

    db synchronize false

--- src/app.module.ts
+++ src/app.module.ts
@@ -25,7 +25,7 @@ import { ResponseTimeMiddleware } from '@nest-middlewares/response-time';
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
       autoLoadEntities: true,
-      synchronize: true,
+      synchronize: false,
       logging: 'all',
     }),
     EmployeesModule,
